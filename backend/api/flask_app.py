31bda4ad141c7561fb887137618273d08b02271d
Len Huang <lenhuang720@gmail.com>
2020-05-14 14:48:46 -0400

    Add homepage

--- backend/api/flask_app.py
+++ backend/api/flask_app.py
@@ -7,6 +7,11 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
+@app.route('/', methods=['GET'])
+def home():
+    return "Welcome to Len and Davis' API"
+
+
 @app.route('/visual', methods=['GET'])
 def visual():
     return jsonify(dummyvisual())
