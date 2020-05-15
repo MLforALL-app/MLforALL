7826694687009fec0358de8de1bb7a3aa41fa1d2
Len Huang <lenhuang720@gmail.com>
2020-05-15 00:59:15 -0400

    redid create-react-app following net_ninja

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-
-import Home from "./components/Home";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Error from "./components/Error";
-import Navigation from "./components/Navigation";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div>
-        <Navigation />
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route component={Error} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<div className="App">
+			<h1> ML For All</h1>
+		</div>
+	);
 }
 
 export default App;
