36a79458c58b6616e3a1ec2b41689ec96f49b77e
Len Huang <lenhuang720@gmail.com>
2020-05-15 04:29:44 -0400

    reactReduxFirebase not a function

--- frontend/src/store/actions/projectActions.js
+++ frontend/src/store/actions/projectActions.js
@@ -1,8 +1,23 @@
 export const createProject = (project) => {
 	return (dispatch, getState, { getFirebase, getFirestore }) => {
 		// make async call to database
-
-		// tehn do this
-		dispatch({ type: "CREATE_PROJECT", project });
+		const firestore = getFirestore();
+		firestore
+			.collection("projects")
+			.add({
+				title: project.title,
+				content: project.content,
+				authorFirstName: "Davis",
+				authorLastName: "Wojo",
+				authorID: "12345",
+				createdAt: new Date()
+			})
+			.then(() => {
+				// tehn do this
+				dispatch({ type: "CREATE_PROJECT", project });
+			})
+			.catch((err) => {
+				dispatch({ type: "CREATE_PROJECT_ERROR", err });
+			});
 	};
 };
