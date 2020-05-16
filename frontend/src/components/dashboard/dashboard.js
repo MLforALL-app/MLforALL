d7a90d22bc4ab8eff53fe62f3eef76440013121f
Len Huang <lenhuang720@gmail.com>
2020-05-15 20:31:11 -0400

    added notifications

--- frontend/src/components/dashboard/dashboard.js
+++ frontend/src/components/dashboard/dashboard.js
@@ -8,7 +8,7 @@ import { Redirect } from "react-router-dom";
 
 class Dashboard extends Component {
 	render() {
-		const { projects, auth } = this.props;
+		const { projects, auth, notifications } = this.props;
 		// Route Protection
 		if (!auth.uid) return <Redirect to="/signin" />;
 		// otws good to go
@@ -19,7 +19,7 @@ class Dashboard extends Component {
 						<ProjectList projects={projects} />
 					</div>
 					<div className="col s12 m5 offset-m1">
-						<Notifications />
+						<Notifications notifications={notifications} />
 					</div>
 				</div>
 			</div>
@@ -31,11 +31,15 @@ const mapStateToProps = (state) => {
 	console.log(state);
 	return {
 		projects: state.firestore.ordered.projects,
-		auth: state.firebase.auth
+		auth: state.firebase.auth,
+		notifications: state.firestore.ordered.notifications
 	};
 };
 
 export default compose(
 	connect(mapStateToProps),
-	firestoreConnect([{ collection: "projects" }])
+	firestoreConnect([
+		{ collection: "projects", orderBy: ["createdAt", "desc"] },
+		{ collection: "notifications", limit: 3, orderBy: ["time", "desc"] }
+	])
 )(Dashboard);
