d1b1425edd1dd68afdbc6c6d85c05efdb268a61a
lenghuang <lendevelops@gmail.com>
2020-05-23 21:47:01 -0400

    changed permissions for view all dashboard

--- frontend/src/components/projects/projectdetails/projectdetails.js
+++ frontend/src/components/projects/projectdetails/projectdetails.js
@@ -9,11 +9,11 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
-import { Redirect } from "react-router-dom";
+// import { Redirect } from "react-router-dom";
 
 const ProjectDetails = (props) => {
 	const { auth, project } = props;
-	if (!auth.uid) return <Redirect to="/signin" />;
+	// if (!auth.uid) return <Redirect to="/signin" />;
 	if (project) {
 		return (
 			<div className="container section project-details">
