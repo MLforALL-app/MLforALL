ce114fd9faae5ca10d15f1be02998c7d7be3a2b2
lenghuang <lendevelops@gmail.com>
2020-05-27 15:53:47 -0400

    uncomment redirect

--- frontend/src/components/projects/projectdetails/projectdetails.js
+++ frontend/src/components/projects/projectdetails/projectdetails.js
@@ -6,7 +6,7 @@ import "./pjdetails.css";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
-// import { Redirect } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 const ProjectDetails = (props) => {
 	const { id, auth, project } = props;
