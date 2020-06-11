0dcf4f5658ed4da10f1c7f70c0ff10da62dd58cf
lenghuang <lendevelops@gmail.com>
2020-06-10 21:47:06 -0400

    add mobile error

--- frontend/src/components/info/mobile.js
+++ frontend/src/components/info/mobile.js
@@ -1,33 +1,34 @@
 import React from "react";
 import logo from "../../pictures/backgrounds/logo.png";
+import oops from "../../pictures/backgrounds/oops.svg";
 
 const Mobile = () => {
 	return (
-		<div
-			style={{
-				position: "fixed",
-				top: "50%",
-				left: "50%",
-				transform: "translate(-50%, -50%)"
-			}}
-			className="container"
-		>
+		<div className="container">
 			<h1 className="purple-text">
-				<img style={{ height: "4.5rem" }} src={logo} alt="" />
+				<img style={{ height: "4rem" }} src={logo} alt="" />
 				forALL
 			</h1>
-			<p>
-				This site is best viewed on desktop and is not supported on
-				mobile. View{" "}
-				<b>
-					<span className="purple-text"> MLforALL </span>
-				</b>{" "}
-				on desktop for the best experience. Check out{" "}
-				<a href="https://www.youtube.com/embed/CswRqTuqzHQ">
-					this video
-				</a>{" "}
-				to learn more.
-			</p>
+			<br />
+			<br />
+			<br />
+			<div>
+				<h4>Sorry.</h4>
+				<br />
+				<p>
+					This site is best viewed on <b>desktop/laptop</b> and is not
+					supported on mobile. Check out{" "}
+					<a href="https://www.youtube.com/embed/CswRqTuqzHQ">
+						this video
+					</a>{" "}
+					to learn more.
+				</p>
+				<br />
+				<br />
+				<br />
+				<br />
+				<img src={oops} alt="" />
+			</div>
 		</div>
 	);
 };
