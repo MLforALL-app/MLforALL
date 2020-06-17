83028d185fbc80ee07a5bcb6ac131cf71841103f
lenghuang <lendevelops@gmail.com>
2020-06-16 20:02:09 -0400

    update backup

--- frontend/src/components/projects/projectList/projectlistbackup.js
+++ frontend/src/components/projects/projectList/projectlistbackup.js
@@ -1,53 +1,8 @@
 import React, { Component } from "react";
-import ProjectSummary from "./projectsummary";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import { Link } from "react-router-dom";
+import FormatList from "./formatList";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
-
-const makeLink = (proj) => {
-	//console.log("THIS IS PROJ FOR MAKELINK", proj);
-	return (
-		<div className="col s12 m6" key={proj.id}>
-			<Link to={`/project/${proj.id}`}>
-				<ProjectSummary project={proj} />{" "}
-			</Link>
-		</div>
-	);
-};
-
-const grouped = (projects) => {
-	const group = [];
-	for (var i = 0; i < projects.length; i++) {
-		if (i < projects.length - 1) {
-			group.push([projects[i], projects[i + 1]]);
-			i++;
-		} else {
-			group.push([projects[i]]);
-		}
-	}
-	//console.log("THIS IS GROUPED", group);
-	return group;
-};
-
-const mapPairs = (pair) => {
-	//console.log("PAIR", pair);
-	if (pair.length < 2) {
-		return (
-			<div className="row" key={pair[0].id}>
-				{makeLink(pair[0])}
-			</div>
-		);
-	} else {
-		return (
-			<div className="row" key={pair[0].id}>
-				{makeLink(pair[0])}
-				{makeLink(pair[1])}
-			</div>
-		);
-	}
-};
 
 class ProjectList extends Component {
 	state = {
@@ -66,17 +21,12 @@ class ProjectList extends Component {
 	}
 	render() {
 		const { projects } = this.props;
+		const { pLoad } = this.state;
 		//console.log("RENDER PROJECT", projects);
 		//console.log("pLOAD", this.state.pLoad);
 		return (
 			<div className="project-list section">
-				{this.state.pLoad ? (
-					grouped(projects).map(mapPairs)
-				) : (
-					<div className="container center">
-						<CircularProgress />
-					</div>
-				)}
+				<FormatList projects={projects} loaded={pLoad} />
 			</div>
 		);
 	}
