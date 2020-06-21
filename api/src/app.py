edf474d537f37ea974eca951109313e704f56b8c
lenghuang <lendevelops@gmail.com>
2020-06-21 02:42:11 -0400

    change host to localhost

--- api/src/app.py
+++ api/src/app.py
@@ -107,4 +107,4 @@ def describe():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(host='127.0.0.1', port=int(os.environ.get('PORT', 8080)))
