da31c12b0cbf6c9c1ad4007959283051a64306ba
lenghuang <lendevelops@gmail.com>
2020-06-21 02:02:07 -0400

    add directory/repo safety checks

--- scripts/mfa.sh
+++ scripts/mfa.sh
@@ -1,19 +1,49 @@
 #!/bin/bash
 
-# To Use This Script, first run these two commands
-# chmod +x mfa.sh 
+# To Use This Script, first do these two things
+# 1) chmod +x mfa.sh 
+# 2) alias `mfa /path/to/your/repo/mfa.sh` (add this to ~/.bashrc)
 # then, you're good to go!
 # be sure to run all these from the root repo folder
 
 function print_help () {
   printf "USAGE (づ｡◕‿‿◕｡)づ\n"
   printf "(note that there will be instances where user input is required)\n"
-  printf "./mfa.sh            ~~>    Show this help display\n"
-  printf "./mfa.sh all        ~~>    Deploy everything\n"
-  printf "./mfa.sh frontend   ~~>    Deploy GHPages and Host on Firebase\n"
-  printf "./mfa.sh api        ~~>    Deploy Python Flask API to Cloud Run\n"
-  printf "./mfa.sh ghpages    ~~>    Deploy only to Github Pages\n"
-  printf "./mfa.sh firebase   ~~>    Deploy and Host on Firebase\n\n"
+  printf "mfa            ~~>    Show this help display\n"
+  printf "mfa all        ~~>    Deploy everything\n"
+  printf "mfa frontend   ~~>    Deploy GHPages and Host on Firebase\n"
+  printf "mfa api        ~~>    Deploy Python Flask API to Cloud Run\n"
+  printf "mfa ghpages    ~~>    Deploy only to Github Pages\n"
+  printf "mfa firebase   ~~>    Deploy and Host on Firebase\n\n"
+}
+
+function check_git() {
+  printf "Checking that this directory contains MLforALL repo... ಠ_ಠ\n"
+  url=`git config --get remote.origin.url`
+  if [ "$url" != "https://github.com/lenghuang/MLforAll.git" ]
+  then
+    printf "\nNoooo wrong repo, bring me back! (ಥ﹏ಥ) \n"
+    error
+    exit 1
+  fi
+}
+
+function check_dir() {
+  printf "Checking you're in the root directory... ಠ╭╮ಠ\n"
+  dir=`pwd`
+  if [ "${dir: -8}" != "MLforAll" ]
+  then
+    printf "\nWrong Directory!!! You're in ...${dir: -32}\n"
+    printf "Go back to /MLforAll! ᕙ(⇀‸↼‶)ᕗ \n"
+    error
+    exit 1
+  fi
+}
+
+function check() {
+  check_git
+  check_dir
+  printf "All checks passed! ~(˘▾˘~) \n\n"
 }
 
 function ghpages_build() {
@@ -58,13 +88,6 @@ function error() {
   printf "\nERROR: ┻━┻ ︵ヽ(\`Д´)ﾉ︵ ┻━┻ \n"
 }
 
-if [[ $1 == "" ]] #Where "$1" is the positional argument you want to validate 
-  then
-  printf "\nWelcome to MLforALL Custom CLI \nby Len Huang ヾ(⌐■_■)ノ♪\n\n"
-  print_help
-  exit 0
-fi
-
 # Parse options to the `./mfa` command
 
 subcommand=$1; shift  # Remove 'mfa' from the argument list
@@ -72,29 +95,41 @@ start
 case "$subcommand" in
   # Parse options to the install sub command
   all ) 
+    check
     firebase
     ghpages_nobuild
     api
     shift $((OPTIND -1))
     ;;
   frontend )
+    check
     firebase
     ghpages_nobuild
     shift $((OPTIND -1))
     ;;
   api )
+    check
     api
     shift $((OPTIND -1))
     ;;
   ghpages )
+    check
     ghpages_build
     shift $((OPTIND -1))
     ;;
   firebase )
+    check
     firebase
     shift $((OPTIND -1))
     ;;
+  "" )
+    check
+    printf "\nWelcome to MLforALL Custom CLI \nby Len Huang ヾ(⌐■_■)ノ♪\n\n"
+    print_help
+    shift $((OPTIND -1))
+    ;;
   * )
+    check
     printf "Invalid Command: $subcommand\n" 1>&2
     shift $((OPTIND -1))
     error
