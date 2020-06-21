096458d3b429e55908e6139ec33ca6cb0dfead69
lenghuang <lendevelops@gmail.com>
2020-06-21 02:54:18 -0400

    remove cols from state (in redux)

--- frontend/src/components/projects/newProject/editpage/modeloutput.js
+++ frontend/src/components/projects/newProject/editpage/modeloutput.js
@@ -7,21 +7,18 @@ import HelpBox from "../../../layouts/helpbox";
 import MenuItem from "@material-ui/core/MenuItem";
 import { updateCurrentWorkingProject } from "../../../../store/actions/projectActions";
 
-
-
-class ModelOutput extends Component{
-    state = {
-        output : "Output",
-        cols : "",
-        loading : true,
-    }
+class ModelOutput extends Component {
+	state = {
+		output: "Output",
+		loading: true
+	};
 	handleDropdownOutput = (event) => {
-        console.log("setting output to ", event.target.value);
-        this.setState({ output: event.target.value });
-        this.props.setOutput(event.target.value);
-    };
+		console.log("setting output to ", event.target.value);
+		this.setState({ output: event.target.value });
+		this.props.setOutput(event.target.value);
+	};
 
-    getMenuItems = (headers) => {
+	getMenuItems = (headers) => {
 		var menuitems = [];
 		headers.forEach((h) => {
 			menuitems.push(
@@ -31,65 +28,57 @@ class ModelOutput extends Component{
 			);
 		});
 		return menuitems;
-    };
-    
-   
+	};
 
-    render() {
-
-        return (
-
-            <div className="row container">
-							<h5>
-								<b>
-									2. In order to predict this output
-									parameter:{" "}
-									<FormControl>
-										<Select
-											value={this.state.output}
-											onChange={this.handleDropdownOutput}
-											displayEmpty
-										>
-											{this.getMenuItems(
-												Object.keys(
-													this.props.cols
-												)
-											)}
-										</Select>
-										<FormHelperText>
-											Output Parameter
-										</FormHelperText>
-									</FormControl>
-									{"  "}
-									<span className="pink-text">
-										<HelpBox
-											header="Output Dropdown"
-											placement="right"
-											desc="Use this dropdown menu to select what column you would like to designate as your output value. This is the parameter that your machine learning model will try to predict!"
-										/>
-									</span>
-								</b>
-							</h5>
-						</div>
-        );
-    };
+	render() {
+		return (
+			<div className="row container">
+				<h5>
+					<b>
+						2. In order to predict this output parameter:{" "}
+						<FormControl>
+							<Select
+								value={this.state.output}
+								onChange={this.handleDropdownOutput}
+								displayEmpty>
+								{this.getMenuItems(
+									Object.keys(this.props.cols)
+								)}
+							</Select>
+							<FormHelperText>Output Parameter</FormHelperText>
+						</FormControl>
+						{"  "}
+						<span className="pink-text">
+							<HelpBox
+								header="Output Dropdown"
+								placement="right"
+								desc="Use this dropdown menu to select what column you would like to designate as your output value. This is the parameter that your machine learning model will try to predict!"
+							/>
+						</span>
+					</b>
+				</h5>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = (state) => {
-    if(state.project.csvData){
-        console.log("Model Output!", state.project.csvData[0]);
-    }
-    
-    return {
-        cols : state.project.csvData && state.project.csvData[0]
-    }
-}
+	if (state.project.csvData) {
+		console.log("Model Output!", state.project.csvData[0]);
+	}
 
-const mapDispatchToProps = (dispatch) => {
 	return {
-		setOutput: (targetParameter) => dispatch(updateCurrentWorkingProject("targetParameter", targetParameter))
+		cols: state.project.csvData && state.project.csvData[0]
 	};
 };
 
+const mapDispatchToProps = (dispatch) => {
+	return {
+		setOutput: (targetParameter) =>
+			dispatch(
+				updateCurrentWorkingProject("targetParameter", targetParameter)
+			)
+	};
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ModelOutput);
