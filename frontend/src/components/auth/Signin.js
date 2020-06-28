26441019d0f3bf2abd27c66684442dfadeb19635
meghaj2 <meghaj2@andrew.cmu.edu>
2020-06-28 19:50:24 -0400

    "Loading ML4ALL" centered/reformatted so the circle is above the text; Also fixed the sign-in image to stick to the bottom of the page

--- frontend/src/components/auth/Signin.js
+++ frontend/src/components/auth/Signin.js
@@ -5,84 +5,92 @@ import { Redirect, Link } from "react-router-dom";
 import authImg from "../../pictures/backgrounds/auth.svg";
 
 class SignIn extends Component {
-	// State to keep track of what user types
-	state = {
-		email: "",
-		password: ""
-	};
+  // State to keep track of what user types
+  state = {
+    email: "",
+    password: "",
+  };
 
-	// eventHandler to update our state the way text has
-	handleChange = (e) => {
-		this.setState({
-			[e.target.id]: e.target.value
-		});
-	};
+  // eventHandler to update our state the way text has
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
 
-	// Call REDUX action signIn to sign user in given state
-	handleSubmit = (e) => {
-		e.preventDefault();
-		this.props.signIn(this.state);
-	};
+  // Call REDUX action signIn to sign user in given state
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.signIn(this.state);
+  };
 
-	render() {
-		// from props, retrieve authError and auth objects
-		const { authError, auth } = this.props;
-		// route protection, shouldn't be able to sign in again
-		if (auth.uid) return <Redirect to="/myprofile" />;
-		return (
-			<div className="signin">
-				<div className="container">
-					<span className="purple-text">
-						<h1>Sign In</h1>
-					</span>
-					<form onSubmit={this.handleSubmit}>
-						<div className="input-field">
-							<label htmlFor="email">Email</label>
-							<input
-								type="email"
-								id="email"
-								onChange={this.handleChange}
-							/>
-						</div>
-						<div className="input-field">
-							<label htmlFor="password">Password</label>
-							<input
-								type="password"
-								id="password"
-								onChange={this.handleChange}
-							/>
-							<div className="purple-text">
-								<br />
-								<Link to="/forgot">Forgot your password?</Link>
-							</div>
-						</div>
-						<div style={{ float: "right" }} className="input-field">
-							<button className="btn z-depth-0 anchor">
-								Login
-							</button>
+  render() {
+    // from props, retrieve authError and auth objects
+    const { authError, auth } = this.props;
+    // route protection, shouldn't be able to sign in again
+    if (auth.uid) return <Redirect to="/myprofile" />;
+    return (
+      <div className="signin">
+        <div className="container">
+          <span className="purple-text">
+            <h1>Sign In</h1>
+          </span>
+          <form onSubmit={this.handleSubmit}>
+            <div className="input-field">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" onChange={this.handleChange} />
+            </div>
+            <div className="input-field">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                onChange={this.handleChange}
+              />
+              <div className="purple-text">
+                <br />
+                <Link to="/forgot">Forgot your password?</Link>
+              </div>
+            </div>
+            <div style={{ float: "right" }} className="input-field">
+              <button className="btn z-depth-0 anchor">Login</button>
 
-							<div className="red-text center">
-								{authError ? <p>{authError}</p> : null}
-							</div>
-						</div>
-					</form>
-				</div>
-				<img className="authImg" alt="" src={authImg}></img>
-			</div>
-		);
-	}
+              <div className="red-text center">
+                {authError ? <p>{authError}</p> : null}
+              </div>
+            </div>
+          </form>
+        </div>
+        <img
+          img="authImg"
+          alt=""
+          src={authImg}
+          id="bgimg"
+          style={{
+            width: "100%",
+            height: "100%",
+            maxHeight: "100%",
+            position: "sticky",
+            bottom: "0px",
+            left: "50%",
+            zIndex: "-1",
+          }}
+        ></img>
+      </div>
+    );
+  }
 }
 
 // Redux to associate state of this component with the props its passed in
 const mapStateToProps = (state, ownProps) => {
-	return { authError: state.auth.authError, auth: state.firebase.auth };
+  return { authError: state.auth.authError, auth: state.firebase.auth };
 };
 
 // Redux to associate action call to a dispatch
 const mapDispatchToProps = (dispatch) => {
-	return {
-		signIn: (creds) => dispatch(signIn(creds))
-	};
+  return {
+    signIn: (creds) => dispatch(signIn(creds)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,63 +9,65 @@ import rootReducer from "./store/reducers/rootReducer";
 import { Provider, useSelector } from "react-redux";
 import thunk from "redux-thunk";
 import {
-	reduxFirestore,
-	getFirestore,
-	createFirestoreInstance
+  reduxFirestore,
+  getFirestore,
+  createFirestoreInstance,
 } from "redux-firestore";
 import {
-	ReactReduxFirebaseProvider,
-	getFirebase,
-	isLoaded
+  ReactReduxFirebaseProvider,
+  getFirebase,
+  isLoaded,
 } from "react-redux-firebase";
 import fbConfig from "./config/fbConfig";
 import firebase from "firebase/app";
 //import cute from "./images/loadingcute.gif";
 
 const store = createStore(
-	rootReducer,
-	compose(
-		applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-		reduxFirestore(firebase, fbConfig)
-	)
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(firebase, fbConfig)
+  )
 );
 
 const rrfConfig = {
-	userProfile: "users",
-	useFirestoreForProfile: true
+  userProfile: "users",
+  useFirestoreForProfile: true,
 };
 
 const rrfProps = {
-	firebase,
-	config: rrfConfig,
-	dispatch: store.dispatch,
-	createFirestoreInstance,
-	presence: "presence",
-	sessions: "sessions"
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance,
+  presence: "presence",
+  sessions: "sessions",
 };
 
 // Update this to update loading screen
 function AuthIsLoaded({ children }) {
-	const auth = useSelector((state) => state.firebase.auth);
-	if (!isLoaded(auth))
-		return (
-			<div className="container center">
-				<CircularProgress />
-				Loading MLforAll
-			</div>
-		);
-	return children;
+  const auth = useSelector((state) => state.firebase.auth);
+  if (!isLoaded(auth))
+    return (
+      <div className="container center" style={{ paddingTop: "10rem" }}>
+        <div className="row">
+          <CircularProgress />
+        </div>
+        Loading MLforAll
+      </div>
+    );
+  return children;
 }
 
 ReactDOM.render(
-	<Provider store={store}>
-		<ReactReduxFirebaseProvider {...rrfProps}>
-			<AuthIsLoaded>
-				<App />
-			</AuthIsLoaded>
-		</ReactReduxFirebaseProvider>
-	</Provider>,
-	document.getElementById("root")
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <AuthIsLoaded>
+        <App />
+      </AuthIsLoaded>
+    </ReactReduxFirebaseProvider>
+  </Provider>,
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
