4ec5d31e3017e51efadb4facf3850d48a993f77a
lenghuang <lendevelops@gmail.com>
2020-06-30 12:59:06 -0400

    install reactplotlyjs and spacing

--- frontend/src/components/education/docs/gaussianNaiveBayesDocs.js
+++ frontend/src/components/education/docs/gaussianNaiveBayesDocs.js
@@ -5,79 +5,82 @@ import Plotly from "plotly.js/dist/plotly-cartesian";
 const PlotlyComponent = createPlotlyComponent(Plotly);
 
 function arange(start, end, step) {
-    var arr = [];
-    for (var i = start; i < end; i += step) {
-        arr.push(i)
-    }
-    return arr;
+	var arr = [];
+	for (var i = start; i < end; i += step) {
+		arr.push(i);
+	}
+	return arr;
 }
 
 function gaussian_density_fn(mu, sig2, x) {
-    return function (x) {
-      return 1.0 / (Math.sqrt(2*Math.PI*sig2)) * Math.exp(-1/(2*sig2)*(x - mu)*(x - mu))
-    }
+	return function (x) {
+		return (
+			(1.0 / Math.sqrt(2 * Math.PI * sig2)) *
+			Math.exp((-1 / (2 * sig2)) * (x - mu) * (x - mu))
+		);
+	};
 }
 
 function makePlot() {
-    let data = [
-        {
-          type: "scatter",
-          x: arange(0, 7, 0.1),
-          y: arange(0, 7, 0.1).map(gaussian_density_fn(3, 0.2)),
-          name: "Female",
-          marker: {
-              color: "rgb(255, 0, 0)",
-          }
-        },
-        {
-            type: "scatter",
-            x: arange(0, 7, 0.1),
-            y: arange(0, 7, 0.1).map(gaussian_density_fn(5, 0.3)),
-            name: "Male",
-            marker: {
-                color: "rgb(0, 0, 255)",
-            }
-          },
-    ];
-    let layout = {
-        // all "layout" attributes: #layout
-        title: "Male/Female Height Distributions", // more about "layout.title": #layout-title
-        xaxis: {
-            showticklabels: false,
-            // all "layout.xaxis" attributes: #layout-xaxis
-            title: "Height", // more about "layout.xaxis.title": #layout-xaxis-title
-        },
-        yaxis: {
-          title: "Probability (Density) of Being That Height"
-        },
-        legend: {
-          bgcolor: "rgba(0, 0, 0, 0)",
-        },
-        paper_bgcolor: "rgb(0, 0, 0, 0)",
-        plot_bgcolor: "rgb(0, 0, 0, 0)",
-    };
-    let config = {
-        showLink: false,
-        displayModeBar: false,
-    };
-    return (
-        <PlotlyComponent
-            className="whatever"
-            data={data}
-            layout={layout}
-            config={config}
-        />
-    );
+	let data = [
+		{
+			type: "scatter",
+			x: arange(0, 7, 0.1),
+			y: arange(0, 7, 0.1).map(gaussian_density_fn(3, 0.2)),
+			name: "Female",
+			marker: {
+				color: "rgb(255, 0, 0)"
+			}
+		},
+		{
+			type: "scatter",
+			x: arange(0, 7, 0.1),
+			y: arange(0, 7, 0.1).map(gaussian_density_fn(5, 0.3)),
+			name: "Male",
+			marker: {
+				color: "rgb(0, 0, 255)"
+			}
+		}
+	];
+	let layout = {
+		// all "layout" attributes: #layout
+		title: "Male/Female Height Distributions", // more about "layout.title": #layout-title
+		xaxis: {
+			showticklabels: false,
+			// all "layout.xaxis" attributes: #layout-xaxis
+			title: "Height" // more about "layout.xaxis.title": #layout-xaxis-title
+		},
+		yaxis: {
+			title: "Probability (Density) of Being That Height"
+		},
+		legend: {
+			bgcolor: "rgba(0, 0, 0, 0)"
+		},
+		paper_bgcolor: "rgb(0, 0, 0, 0)",
+		plot_bgcolor: "rgb(0, 0, 0, 0)"
+	};
+	let config = {
+		showLink: false,
+		displayModeBar: false
+	};
+	return (
+		<PlotlyComponent
+			className="whatever"
+			data={data}
+			layout={layout}
+			config={config}
+		/>
+	);
 }
 
 const gaussianNaiveBayes = (
-    <div>
-        {header("Gaussian Naive Bayes", "+2")}
-        {makePlot()}
+	<div>
+		{header("Gaussian Naive Bayes", "+2")}
+		{makePlot()}
 
-        {header("Technical Explanation")}
+		{header("Technical Explanation")}
 
-        {bodyContent(`
+		{bodyContent(`
 Let $x = (x_1, \\dots, x_n)$ be feature vector with label $y$.
 From Bayes' theorem, we have
 $$\\begin{equation*}
@@ -101,7 +104,7 @@ $$\\begin{equation*}
 \\end{equation*}$$
 In Gaussian Naive Bayes, we assume $Pr(x_i\\mid y)\\sim \\mathcal{N}(\\mu_y, \\sigma_y^2)$ where $\\mu_y$ and $\\sigma_y^2$ are estimated using maximum likelihood estimation.
 `)}
-    </div>
+	</div>
 );
 
 export default gaussianNaiveBayes;
--- frontend/src/components/education/docs/logisticRegressionDocs.js
+++ frontend/src/components/education/docs/logisticRegressionDocs.js
@@ -20,81 +20,81 @@ function logistic_fn(beta, c, x) {
 }
 
 function makePlot() {
-    let data = [
-        {
-            type: "scatter", // all "scatter" attributes: https://plot.ly/javascript/reference/#scatter
-            mode: "markers",
-            x: arange(3, 6, 1), // more about "x": #scatter-x
-            y: [1, 1, 1], // #scatter-y
-            name: "Yes",
-            marker: {
-                // marker is an object, valid marker keys: #scatter-marker
-                color: "rgb(255, 0, 0)", // more about "marker.color": #scatter-marker-color
-                size: 10,
-            },
-        },
-        {
-          type: "scatter", // all "scatter" attributes: https://plot.ly/javascript/reference/#scatter
-          mode: "markers",
-          x: arange(1, 4, 1), // more about "x": #scatter-x
-          y: [0, 0, 0], // #scatter-y
-          name: "No",
-          marker: {
-              // marker is an object, valid marker keys: #scatter-marker
-              color: "rgb(0, 0, 255)", // more about "marker.color": #scatter-marker-color
-              size: 10,
-          },
-        },
-        {
-          type: "scatter",
-          x: arange(0, 7, 0.1),
-          y: arange(0, 7, 0.1).map(logistic_fn(3.0, 3)),
-          name: "Probability of Liking Math",
-          marker: {
-              color: "rgb(0, 0, 0)",
-          }
-        },
-    ];
-    let layout = {
-        // all "layout" attributes: #layout
-        title: "Probability of Liking Math Given Number of Math Classes Taken", // more about "layout.title": #layout-title
-        xaxis: {
-            // all "layout.xaxis" attributes: #layout-xaxis
-            title: "Number of Math Classes Taken in College", // more about "layout.xaxis.title": #layout-xaxis-title
-        },
-        yaxis: {
-          title: "Likes Math"
-        },
-        legend: {
-          bgcolor: "rgba(0, 0, 0, 0)",
-        },
-        paper_bgcolor: "rgba(0, 0, 0, 0)",
-        plot_bgcolor: "rgba(0, 0, 0, 0)",
-    };
-    let config = {
-        showLink: false,
-        displayModeBar: false,
-    };
-    return (
-        <PlotlyComponent
-            className="whatever"
-            data={data}
-            layout={layout}
-            config={config}
-        />
-    );
+	let data = [
+		{
+			type: "scatter", // all "scatter" attributes: https://plot.ly/javascript/reference/#scatter
+			mode: "markers",
+			x: arange(3, 6, 1), // more about "x": #scatter-x
+			y: [1, 1, 1], // #scatter-y
+			name: "Yes",
+			marker: {
+				// marker is an object, valid marker keys: #scatter-marker
+				color: "rgb(255, 0, 0)", // more about "marker.color": #scatter-marker-color
+				size: 10
+			}
+		},
+		{
+			type: "scatter", // all "scatter" attributes: https://plot.ly/javascript/reference/#scatter
+			mode: "markers",
+			x: arange(1, 4, 1), // more about "x": #scatter-x
+			y: [0, 0, 0], // #scatter-y
+			name: "No",
+			marker: {
+				// marker is an object, valid marker keys: #scatter-marker
+				color: "rgb(0, 0, 255)", // more about "marker.color": #scatter-marker-color
+				size: 10
+			}
+		},
+		{
+			type: "scatter",
+			x: arange(0, 7, 0.1),
+			y: arange(0, 7, 0.1).map(logistic_fn(3.0, 3)),
+			name: "Probability of Liking Math",
+			marker: {
+				color: "rgb(0, 0, 0)"
+			}
+		}
+	];
+	let layout = {
+		// all "layout" attributes: #layout
+		title: "Probability of Liking Math Given Number of Math Classes Taken", // more about "layout.title": #layout-title
+		xaxis: {
+			// all "layout.xaxis" attributes: #layout-xaxis
+			title: "Number of Math Classes Taken in College" // more about "layout.xaxis.title": #layout-xaxis-title
+		},
+		yaxis: {
+			title: "Likes Math"
+		},
+		legend: {
+			bgcolor: "rgba(0, 0, 0, 0)"
+		},
+		paper_bgcolor: "rgba(0, 0, 0, 0)",
+		plot_bgcolor: "rgba(0, 0, 0, 0)"
+	};
+	let config = {
+		showLink: false,
+		displayModeBar: false
+	};
+	return (
+		<PlotlyComponent
+			className="whatever"
+			data={data}
+			layout={layout}
+			config={config}
+		/>
+	);
 }
 
 const logisticRegression = (
-    <div>
-        {header("Logistic Regression", "+2")}
+	<div>
+		{header("Logistic Regression", "+2")}
 
-        {bodyContent(`
+		{bodyContent(`
 Logistic regression is used to estimate the probability that objects belong to different categories based on information about those objects. For example, we could measure the height and weight of an animal and give the probability that the animal is a dog versus a cat. In the example below, we estimate the probability that a person likes math based on the number of math classes they've taken.     
 `)}
-        {makePlot()}
+		{makePlot()}
 
-        {bodyContent(`
+		{bodyContent(`
 The function above used to estimate the probability of liking math is the "sigmoid" or "logistic" function $g(x) = \\frac{1}{1+e^{-x}}$. During training, the model learns parameters $\\theta_0,\\dots,\\theta_n$ associated with each input variable so that the predicted probability of liking math is 
 $$
 \\begin{equation}
@@ -104,8 +104,8 @@ $$
 
 While we're only discussing logistic regression in the context of two possible categories, it can be extended to more than two categories as well.
 `)}
-        {header("Technical Explanation")}
-        {bodyContent(`
+		{header("Technical Explanation")}
+		{bodyContent(`
 Notice that $g(z) \\in (0, 1)$. We want a model where $p(y=1\\mid x; \\theta) = \\hat{y}$. It follows that $p(y=0\\mid x; \\theta) = 1 - \\hat{y}$. Another way of saying this is
 $$\\begin{equation}
 p(y\\mid x; \\theta) = (\\hat{y})^{y}(1-\\hat{y})^{1-y}
