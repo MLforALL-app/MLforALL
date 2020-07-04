8892eed9a934c7153c528fbf185bc3e5af91ec8c
lenghuang <lendevelops@gmail.com>
2020-07-04 02:57:57 -0400

    switch back to testing db

--- frontend/src/config/collection.js
+++ frontend/src/config/collection.js
@@ -1,5 +1,5 @@
 // Test Collection in Firestore
 const projectSource = "projects";
-// const projectSource = "projects-production";
+//const projectSource = "projects-production";
 
 export { projectSource as default };
