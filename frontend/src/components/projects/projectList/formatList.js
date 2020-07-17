e0472aa2bd4f8aaca4247421c3cdcf8a09235feb
meghaj2 <meghaj2@andrew.cmu.edu>
2020-07-16 21:15:18 -0400

    Cards include image, three per row

--- frontend/src/components/projects/projectList/formatList.js
+++ frontend/src/components/projects/projectList/formatList.js
@@ -4,55 +4,84 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import { Link } from "react-router-dom";
 
 const makeLink = (proj) => {
-	return (
-		<div className="col s12 m6" key={`link_${proj.pid}`}>
-			<Link to={`/project/${proj.pid}`}>
-				<ProjectSummary project={proj} />{" "}
-			</Link>
-		</div>
-	);
+  return (
+    <div className="col s12 m4" key={`link_${proj.pid}`}>
+      <Link to={`/project/${proj.pid}`}>
+        <ProjectSummary project={proj} />{" "}
+      </Link>
+    </div>
+  );
 };
 
 const grouped = (projects) => {
-	const group = [];
-	for (var i = 0; i < projects.length; i++) {
-		if (i < projects.length - 1) {
-			group.push([projects[i], projects[i + 1]]);
-			i++;
-		} else {
-			group.push([projects[i]]);
-		}
-	}
-	return group;
+  const group = [];
+  for (var i = 0; i < projects.length; i++) {
+    if (i < projects.length - 2) {
+      group.push([projects[i], projects[i + 1], projects[i + 2]]);
+      i += 3;
+    } else if (i < projects.length - 1) {
+      group.push([projects[i], projects[i + 1]]);
+      i += 2;
+    } else {
+      group.push([projects[i]]);
+      i++;
+    }
+  }
+  return group;
 };
 
-const mapPairs = (pair) => {
-	if (pair.length < 2) {
-		return (
-			<div className="row" key={`row_${pair[0].pid}`}>
-				{makeLink(pair[0])}
-			</div>
-		);
-	} else {
-		return (
-			<div className="row" key={`row_${pair[0].pid}`}>
-				{makeLink(pair[0])}
-				{makeLink(pair[1])}
-			</div>
-		);
-	}
+// const mapPairs = (pair) => {
+// 	if (pair.length < 2) {
+// 		return (
+// 			<div className="row" key={`row_${pair[0].pid}`}>
+// 				{makeLink(pair[0])}
+// 			</div>
+// 		);
+// 	} else {
+// 		return (
+// 			<div className="row" key={`row_${pair[0].pid}`}>
+// 				{makeLink(pair[0])}
+// 				{makeLink(pair[1])}
+// 			</div>
+// 		);
+// 	}
+// };
+
+const mapTriples = (trip) => {
+  if (trip.length < 2) {
+    return (
+      <div className="row" key={`row_${trip[0].pid}`}>
+        {makeLink(trip[0])}
+      </div>
+    );
+  } else if (trip.length < 3) {
+    return (
+      <div className="row" key={`row_${trip[0].pid}`}>
+        {makeLink(trip[0])}
+        {makeLink(trip[1])}
+      </div>
+    );
+  } else {
+    return (
+      <div className="row" key={`row_${trip[0].pid}`}>
+        {makeLink(trip[0])}
+        {makeLink(trip[1])}
+        {makeLink(trip[2])}
+      </div>
+    );
+  }
 };
 
 const FormatList = ({ projects }) => {
-	if (projects) {
-		return grouped(projects).map(mapPairs);
-	} else {
-		return (
-			<div className="container center">
-				<CircularProgress />
-			</div>
-		);
-	}
+  if (projects) {
+    return grouped(projects).map(mapTriples);
+  } else {
+    return (
+      <div className="container center">
+        <CircularProgress />
+      </div>
+    );
+  }
 };
 
 export default FormatList;
--- frontend/src/components/projects/projectList/projectsummary.js
+++ frontend/src/components/projects/projectList/projectsummary.js
@@ -1,34 +1,43 @@
 import React from "react";
 import moment from "moment";
+import img1 from "../../../pictures/landing/LP4.svg";
 //issue number 60 add banner/background
 const ProjectSummary = ({ project }) => {
-	const shorten = (s) => {
-		return s.length < 15 ? s : s.substr(0, 13) + "..";
-	};
-	return (
-		<div className="project-summary">
-			<div className="card z-depth-1" style={{ maxWidth: "500px" }}>
-				<div className="card-content content-sum">
-					<span className="card-title"> {project.title} </span>
-					<p>
-						{project.content.length > 100
-							? project.content.substr(0, 100) + "..."
-							: project.content}
-					</p>
-				</div>
-				<div className="card-action">
-					<div style={{ float: "left" }}>
-						{moment(project.createdAt.toDate()).calendar()}
-					</div>
-					<div style={{ float: "right" }}>
-						{shorten(project.authorFirstName + " " + project.authorLastName)}{" "}
-						<span className="hearts">&hearts;</span>
-					</div>
-					<div style={{ clear: "both" }}></div>
-				</div>
-			</div>
-		</div>
-	);
+  const shorten = (s) => {
+    return s.length < 15 ? s : s.substr(0, 13) + "..";
+  };
+
+  //   const background = () => {
+  // 	  return {project ? project.backgroundImage : `url(${img1})`}
+  //   }
+  return (
+    <div className="project-summary">
+      <div className="card z-depth-1" style={{ maxWidth: "325px" }}>
+        <div
+          className="card-picture"
+          style={{ backgroundImage: `url(${img1})`, backgroundColor: "white" }}
+        ></div>
+        <div className="card-content content-sum">
+          <span className="card-title"> {project.title} </span>
+          <p>
+            {project.content.length > 100
+              ? project.content.substr(0, 100) + "..."
+              : project.content}
+          </p>
+        </div>
+        <div className="card-action">
+          <div style={{ float: "left" }}>
+            {moment(project.createdAt.toDate()).calendar()}
+          </div>
+          <div style={{ float: "right" }}>
+            {shorten(project.authorFirstName + " " + project.authorLastName)}{" "}
+            <span className="hearts">&hearts;</span>
+          </div>
+          <div style={{ clear: "both" }}></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectSummary;
