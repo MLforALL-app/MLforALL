41f172f538caaa31fdab9c105af230e6445f3d53
Len Huang <lendevelops@gmail.com>
2020-08-09 22:31:08 -0400

    fix unused imports

--- frontend/src/components/auth/form.js
+++ frontend/src/components/auth/form.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
 import DraftsIcon from "@material-ui/icons/Drafts";
 import MailIcon from "@material-ui/icons/Mail";
 import { signUp, signIn } from "../../store/actions/authActions";
